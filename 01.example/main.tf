37bf84370ba093df764884577c6e8ca916ded7d3
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-17 17:32:28 +0530

    updating learning-terraform

---
+++ 01.example/main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "web" {
+  ami           = "ami-07418f7e626609d5b"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "Avinash"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-0866353c733e68495"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
