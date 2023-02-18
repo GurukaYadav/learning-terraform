249e07a6d5d70e1dffddd2e7b6e6096c60e29e52
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-18 16:19:47 +0530

    updating learning-terraform

--- 05.loops/01.approach-1/main.tf
+++ 05.loops/01.approach-1/main.tf
@@ -1,5 +1,10 @@
+variable "no-of-instances" {
+  default = 2
+}
+
 resource "aws_instance" "web" {
-  count         = 2
+
+  count         = var.no-of-instances
   ami           = "ami-07418f7e626609d5b"
   instance_type = "t3.micro"
 
