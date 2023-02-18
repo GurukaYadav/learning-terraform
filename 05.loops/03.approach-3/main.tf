9109018e8ccc34259fa38d81f9d1cd6ae5a6d27b
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-18 16:50:18 +0530

    updating learning-terraform

---
+++ 05.loops/03.approach-3/main.tf
@@ -0,0 +1,17 @@
+//sometimes we need to iterate the loop based on the no of inputs
+//so here i want to change the instance_types for 2 instances
+
+resource "aws_instance" "web" {
+
+  count         = length(var.instance_type)
+  ami           = "ami-07418f7e626609d5b"
+  instance_type = var.instance_type[count.index]
+
+  tags = {
+    Name = "Avinash-${count.index+1}"
+  }
+}
+
+variable "instance_type" {
+  default = [t3.micro, t2.small]
+}
