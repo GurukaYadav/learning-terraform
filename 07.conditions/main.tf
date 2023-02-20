8b31f668647bc0923a40abbe0e9cf57fe8e31a8a
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 14:57:04 +0530

    updating learning-terraform

--- 07.conditions/main.tf
+++ 07.conditions/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-//count = 0 (here count is used whether to create the instances or not//)
-  instance = var.instance_type == null ? "t3.micro" : var.instance_type
+//count = 0 (here count is used whether to create the instances or not)
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
   ami      = "ami-07418f7e626609d5b"
 
   tags = {
