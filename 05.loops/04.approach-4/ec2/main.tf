b9e8e20fd5eca6f040b070cc4336739a5eb0eab3
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 13:57:26 +0530

    updating learning-terraform

---
+++ 05.loops/04.approach-4/ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  count         = var.instance_count
+  ami           = "ami-07418f7e626609d5b"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Avinash"
+  }
+}
+
+variable "instance_type" {}
+variable "instance_count" {}
