b6c1055a1d6e528c24f17c52b86962a5d5346dba
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 16:38:52 +0530

    updating learning-terraform

---
+++ 08.provisioners/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "web" {
+  ami           = "ami-07418f7e626609d5b"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = Avinash
+  }
+
+  connection {
+      type     = "ssh"
+      user     = "root"
+      password = DevOps321
+      host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "uname",
+    ]
+  }
+}
