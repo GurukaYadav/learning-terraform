16d0a8a5f6701761cfe01d9594b082aa34e31989
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 15:02:58 +0530

    updating learning-terraform

--- 07.conditions/main.tf
+++ 07.conditions/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-//count = 0 (here count is used whether to create the instances or not)
+//count = 0 (here count is used whether to create the instances or not and also we can create the required no.of instances based on the count)
   instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
   ami      = "ami-07418f7e626609d5b"
 
@@ -11,3 +11,5 @@ resource "aws_instance" "web" {
 variable "instance_type" {
   default = null
 }
+
+//here the null value can be overwritten using -var instance_type="" during terraform apply
