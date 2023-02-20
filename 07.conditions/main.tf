dccc2e2744aaa0c939448a67c630749438a2b835
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 14:47:06 +0530

    updating learning-terraform

---
+++ 07.conditions/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "web" {
+  count = 0 (//here count is used whether to create  the instances or not//)
+  instance = var.instance_type == null ? "t3.micro"  : var.instance_type
+  ami           = "ami-07418f7e626609d5b"
+
+  tags = {
+    Name = "Avinash"
+  }
+}
+
+variable "instance_type" {
+  default = null
+}
