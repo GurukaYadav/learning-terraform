b8420e7452d6482bbbe444481fee5f18fd131beb
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 20:01:55 +0530

    updating learning-terraform

--- 09.data-sources/main.tf
+++ 09.data-sources/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "web" {
-  ami           = "ami-07418f7e626609d5b"
+  ami           = "data.aws_ami.ami.image_id"
   instance_type = "t3.micro"
   tags = {
     Name = "Avinash"
   }
 }
 
-//data "aws_ami" "ami" {
-//  executable_users = ["self"]
-//  most_recent      = true
-// name_regex       = "base-with-ansible"
-// owners           = ["self"]
-//}
+data "aws_ami" "ami" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "base-with-ansible"
+  owners           = ["self"]
+}
