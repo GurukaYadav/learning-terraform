5de8c6b6b1e9f84afde6b1116b9a0c9e3533c555
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 13:10:35 +0530

    updating learning-terraform

--- 05.loops/04.approach-4/main.tf
+++ 05.loops/04.approach-4/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
 
   count         = length(var.instance_type)
   ami           = "ami-07418f7e626609d5b"
-  instance_type = var.instance_type[count.index]
+  instance_type = var.instance_type
 
   tags = {
     Name = "Avinash-${count.index+1}"
