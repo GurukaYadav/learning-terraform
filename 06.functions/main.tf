48be38237601531567935b1877335c73d7608a70
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-20 12:46:40 +0530

    updating learning-terraform

--- 06.functions/main.tf
+++ 06.functions/main.tf
@@ -1,13 +1,18 @@
 variable "list" {
   default = ["t2.micro", "t4.micro"]
 }
+
 output "list" {
   value = length(var.list)
+}
 
 variable "map" {
   default = {
     "t2.micro" = 2,
     "t4.micro" = 1
+  }
 }
+
 output "map" {
   value = length(var.map)
+}
