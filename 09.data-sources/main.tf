1a620ceaae053a24aff7e32e932e920b3fe54dbd
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-22 17:40:15 +0530

    updating learning-terraform

--- 09.data-sources/main.tf
+++ 09.data-sources/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
 }
 
 data "aws_ami" "example" {
-//  executable_users = ["self"]
+  executable_users = ["self"]
   most_recent      = true
   name_regex       = "base-with-ansible"
   owners           = ["self"]
